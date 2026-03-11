739eb8d96b8b2b2a533d3133d943e05d1eda76fa
xor-kajul <kajul.verma@xoriant.com>
2026-03-11 13:21:57 +0530

    added validation in app

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-var number=12; // declared the number to be guessed by user
+var number=12; // declared the number to be guessed by user 
+var myName = "Kajul";
 
-alert("Welcome to the Number Guessing Game!");
+alert("Welcome to the Number Guessing Game :"+" "+myName);
+
 
 var guessedCorrectly = false; 
 
-while (!guessedCorrectly) {
-
-	var guess = prompt('Enter your guess?');
-		
-		if(guess == number)
+while (!guessedCorrectly) 
+	{
+	
+	var guess = parseInt(prompt('Enter your guess?')); // Prompt the user to enter their guess and convert it to an integer
+	
+	if(guess === number)
 		{
-
-		alert("You guessed correctly!");
-        
-		guessedCorrectly = true; // Exit the loop
-
+			
+			alert("You guessed correctly!");
+			
+			guessedCorrectly = true; // Exit the loop
+			
 		}
 		
 		else 
-        {
-       
-        alert("Oops!!Incorrect");
-        
-        }
-
-}
+			{
+				if(typeof guess !== 'number' || isNaN(guess)) // Check if the input is not a number
+				{
+					alert("Please enter a valid number.");
+					continue; // Skip the rest of the loop and prompt again
+				}
+				
+				
+				alert("Oops!!Incorrect");
+				
+			}
+			
+	}
